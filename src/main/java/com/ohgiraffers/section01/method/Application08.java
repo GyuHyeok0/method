16d6259040663516a7374206c4b5ec446a85f4a3
pyunghun <564564q@naver.com>
2024-03-07 12:03:45 +0900

    method267

--- src/main/java/com/ohgiraffers/section01/method/Application08.java
+++ src/main/java/com/ohgiraffers/section01/method/Application08.java
@@ -14,6 +14,7 @@ public class Application08 {
         int second = 30;
         System.out.println( Application08.sumTwoNumbers(first,second));
 
+        System.out.println( Application08.sumTwoNumbers(first,second));
 //         필기. 스테틱은 코드가 실행 될때 공간을 만들기 때문에 미리 공간을 만들 필요가 없다.
 //         필기. 동일한 클래스 내에 작성된 static 메소드는 클래스명이 생략 가능하다.
         System.out.println(sumTwoNumbers(20,30));
